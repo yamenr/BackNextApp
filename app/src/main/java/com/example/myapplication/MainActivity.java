648c44e43c2b105462cb4ed8f5ea0fbe5384770a
Mohamed Zabi <mohamdzoabi1@gmail.com>
2023-09-30 09:14:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void gotoBack(View view) {
+        Intent i = new Intent(this, BackActivity.class);
+        startActivity(i);
+    }
+}
